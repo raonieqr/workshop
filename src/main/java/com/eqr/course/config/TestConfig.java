1a9e462050488869e0f355dfe60b80f9289d4b3c
raonieqr <raoniestevan@gmail.com>
2023-07-15 19:08:45 -0300

    add: in ISO 8601

--- src/main/java/com/eqr/course/config/TestConfig.java
+++ src/main/java/com/eqr/course/config/TestConfig.java
@@ -1,12 +1,15 @@
 package com.eqr.course.config;
 
+import com.eqr.course.entities.Order;
 import com.eqr.course.entities.User;
+import com.eqr.course.repositories.OrderRepository;
 import com.eqr.course.repositories.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 @Configuration
@@ -14,11 +17,16 @@ import java.util.Arrays;
 public class TestConfig implements CommandLineRunner{
     @Autowired
     private UserRepository userRepository;
-
+    @Autowired
+    private OrderRepository orderRepository;
     @Override
     public void run(String... args) throws Exception {
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "1234567", "012345");
         User u2 = new User(null, "Alex Brown", "alex@gmail.com", "2345678", "123456");
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
+
         userRepository.saveAll(Arrays.asList(u1, u2));
+        orderRepository.saveAll(Arrays.asList(o1, o2));
     }
 }
