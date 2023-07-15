aa4c1501cc3757b5f3bd44ef5d4be7bbd4b7d7f0
raonieqr <raoniestevan@gmail.com>
2023-07-14 21:32:49 -0300

    add: Order

--- src/main/java/com/eqr/course/entities/User.java
+++ src/main/java/com/eqr/course/entities/User.java
@@ -3,6 +3,8 @@ package com.eqr.course.entities;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -19,6 +21,8 @@ public class User implements Serializable
 	private String phone;
 	private String password;
 
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	public User() {}
 	public User(Long id, String name, String email, String phone, String password)
 	{
@@ -77,6 +81,10 @@ public class User implements Serializable
 	public void setPassword(String password)
 	{
 		this.password = password;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	@Override
