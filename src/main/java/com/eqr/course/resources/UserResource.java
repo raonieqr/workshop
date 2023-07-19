3159466b751032d81b76091b71717df1d2fc1553
raonieqr <raoniestevan@gmail.com>
2023-07-18 21:07:22 -0300

    add: insert user

--- src/main/java/com/eqr/course/resources/UserResource.java
+++ src/main/java/com/eqr/course/resources/UserResource.java
@@ -4,11 +4,10 @@ import com.eqr.course.entities.User;
 import com.eqr.course.services.UserServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 
@@ -27,5 +26,12 @@ public class UserResource {
 	public ResponseEntity<User> findById(@PathVariable Long id) {
 		User obj = services.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+
+	@PostMapping
+	public ResponseEntity<User> inset(@RequestBody User obj) {
+		obj = services.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
 	}
 }
