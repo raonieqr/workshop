1ef5b693fc1955a19246081a0d35e4a845fdc1aa
raonieqr <raoniestevan@gmail.com>
2023-07-19 16:37:31 -0300

    add: exceptions for database

--- src/main/java/com/eqr/course/services/UserServices.java
+++ src/main/java/com/eqr/course/services/UserServices.java
@@ -2,8 +2,11 @@ package com.eqr.course.services;
 
 import com.eqr.course.entities.User;
 import com.eqr.course.repositories.UserRepository;
+import com.eqr.course.services.exceptions.DatabaseException;
 import com.eqr.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -27,7 +30,15 @@ public class UserServices {
     }
 
     public void delete(Long id) {
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        }
+        catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        }
+        catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj) {
