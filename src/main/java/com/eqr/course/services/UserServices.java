b400022f2ab78e7e096651ddcd9dfc31ddc3bfa6
raonieqr <raoniestevan@gmail.com>
2023-07-19 16:26:20 -0300

    add: exceptions

--- src/main/java/com/eqr/course/services/UserServices.java
+++ src/main/java/com/eqr/course/services/UserServices.java
@@ -2,6 +2,7 @@ package com.eqr.course.services;
 
 import com.eqr.course.entities.User;
 import com.eqr.course.repositories.UserRepository;
+import com.eqr.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,7 +19,7 @@ public class UserServices {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
