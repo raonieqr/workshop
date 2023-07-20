72ac39a3932b5d3893a1dafa61b3aa12c7015780
raonieqr <raoniestevan@gmail.com>
2023-07-20 12:54:37 -0300

    add: update exception

--- src/main/java/com/eqr/course/services/UserServices.java
+++ src/main/java/com/eqr/course/services/UserServices.java
@@ -4,6 +4,7 @@ import com.eqr.course.entities.User;
 import com.eqr.course.repositories.UserRepository;
 import com.eqr.course.services.exceptions.DatabaseException;
 import com.eqr.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -42,9 +43,14 @@ public class UserServices {
     }
 
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        }
+        catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj)
